cd46ca9724ffdbfb95e0c13352507c851bf39b8f
AlexDVK <devksh@abiatec.com>
2016-08-31 11:03:05 +0300

    Add project

---
+++ src/js/partials/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    var config = {
+        '.chosen-select': {},
+        '.chosen-select-deselect': {allow_single_deselect: true},
+        '.chosen-select-no-single': {disable_search_threshold: 10},
+        '.chosen-select-no-results': {no_results_text: 'Oops, nothing found!'},
+        '.chosen-select-width': {width: "95%"}
+    }
+    for (var selector in config) {
+        $(selector).chosen(config[selector]);
+    }
+
+    $('.main-slider .bxslider').bxSlider();
+
+
+    $('.menu-button').click(
+        function (e) {
+            e.preventDefault();
+            $('#nav-icon3').toggleClass('open');
+            $('.menu-dropdown').toggle();
+        })
+
+    $(".fancybox").fancybox();
+});
+
+
